ad9dc044d6f569442be20bb0756b4723a4497954
Dakota Sliter <djsliter@gmail.com>
2023-02-15 15:01:01 -0500

    gdg

--- projects/sierpinski_click/sierpinski_click.js
+++ projects/sierpinski_click/sierpinski_click.js
@@ -2,13 +2,10 @@
 
 var canvas;
 var gl;
-
-var x1 = -1;
-var x2 = 0;
-var x3 = 1;
-var y1 = -1;
-var y2 = 1;
-var y3 = -1;
+var coords =  [ [-1 , -1] , [0 , 1] , [1 , -1]];
+ 
+var mouseDown = false;
+var closestVertex = 0;
 
 var points = [];
 
@@ -17,35 +14,58 @@ var numTimesToSubdivide = 0;
 function init()
 {
     canvas = document.getElementById( "gl-canvas" );
-    canvas.addEventListener("mouseup", function(event) {
-        var rect = gl.canvas.getBoundingClientRect();
-        var newx = (event.clientX - rect.left) / canvas.width * 2 - 1;
-        var newy = (event.clientY - rect.top) / canvas.height * -2 + 1;
-        
-        var vertex_id = document.querySelector('input[name="vertex"]:checked').value;
-        if (vertex_id == 0) {
-            x1 = newx;
-            y1 = newy;
-        } else if (vertex_id == 1) {
-            x2 = newx;
-            y2 = newy;
-        } else {
-            x3 = newx;
-            y3 = newy;
-        }
-
-
-        console.log(newx, newy);
-    });
-
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
+    var rect = gl.canvas.getBoundingClientRect();
 
+    canvas.addEventListener("mousedown", (e) => {
+        // Calculate closest vertex
+        var dist = 100;
+        var currDist;
+        var x = (e.clientX - rect.left) / canvas.width * 2 - 1;
+        var y = (e.clientY - rect.top) / canvas.height * -2 + 1;
+        
+        for(let i = 0; i < coords.length; i++) {
+            var x2 = Math.pow((coords[i][0] - x), 2);
+            var y2 = Math.pow((coords[i][1] - y), 2);
+            var sum = x2 + y2;
+
+            var currDist = Math.sqrt(sum);
+            
+            if (currDist < dist) {
+                dist = currDist;
+                closestVertex = i;
+            }
+        } 
+        
+        mouseDown = true;
+    });
+    canvas.addEventListener("mouseup", (e) => {
+        mouseDown = false;
+    });
+
+    canvas.addEventListener("mousemove", (e) => {
+        if (mouseDown == true) {
+            var newx = (e.clientX - rect.left) / canvas.width * 2 - 1;
+            var newy = (e.clientY - rect.top) / canvas.height * -2 + 1;
+
+            if (closestVertex == 0) {
+                coords[0][0] = newx;
+                coords[0][1] = newy;
+            } else if (closestVertex == 1) {
+                coords[1][0] = newx;
+                coords[1][1] = newy;
+            } else {
+                coords[2][0] = newx;
+                coords[2][1] = newy;
+            }
+        }
+    });
     // First, initialize the corners of our gasket with three points.
     var vertices = [
-        vec2( x1, y1 ),
-        vec2( x2, y2 ),
-        vec2( x3, y3 )
+        vec2( coords[0][0], coords[0][1] ),
+        vec2( coords[1][0], coords[1][1] ),
+        vec2( coords[2][0], coords[2][1] )
     ];
 
     divideTriangle( vertices[0], vertices[1], vertices[2],
