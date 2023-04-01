2d0574532b258ba422711491e89b23a7a1a94b06
Dakota Sliter <djsliter@gmail.com>
2023-04-01 11:27:10 -0400

    editing space game

--- projects/spaceship_three/js/spacegame.js
+++ projects/spaceship_three/js/spacegame.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js'
 
 let stars = []
+const starGroup = new THREE.Group()
 let pitchAxis = new THREE.Vector3(1, 0, 0);
 let rollAxis = new THREE.Vector3(0, 0, 1);
 let direction = new THREE.Vector3();
@@ -17,10 +18,15 @@ class Ship {
                 scene.add( gltf.scene );
                 gltf.scene.scale.set(0.25, 0.25, 0.25)
                 gltf.scene.rotation.set(0.0, 3.14, 0);
+                gltf.scene.add( camera )
+                camera.lookAt(gltf.scene)
+                camera.position.set( 0, 1000, -3000 );
+                camera.rotation.set(0.2, Math.PI, 0)
                 this.ship = gltf.scene;
             }
 
         ); 
+        
     }
 
     rotateRight() {
@@ -30,12 +36,8 @@ class Ship {
         this.ship.rotateOnAxis(rollAxis, -0.04)
     }
     moveForward() {
-        this.ship.getWorldDirection(direction);
-        camera.position.x = this.ship.position.x
-        camera.position.y = this.ship.position.y
-        camera.position.z = this.ship.position.z + 700
-        camera.lookAt(this.ship.position)
-        this.ship.position.add(direction.multiplyScalar(10.0))
+        starGroup.position.add(direction.multiplyScalar(-10.0))
+        console.log(starGroup)
     }
     pitchUp() {
         this.ship.rotateOnAxis(pitchAxis, -0.04)
@@ -44,17 +46,14 @@ class Ship {
         this.ship.rotateOnAxis(pitchAxis, 0.04)
     }
     drift() {
-        this.ship.getWorldDirection(direction);
-        camera.position.x = this.ship.position.x
-        camera.position.y = this.ship.position.y
-        camera.position.z = this.ship.position.z + 700
-        camera.lookAt(this.ship.position)
-        this.ship.position.add(direction.multiplyScalar(3.0))
+        stars.position.add(direction.multiplyScalar(-3.0))
     }
 }
 
 // Used to add primitives to the scene
 const { scene, camera, renderer, loader, ship } = init();
+addSphere();
+scene.add(starGroup)
 
 
 var forwardActionID;
@@ -114,7 +113,7 @@ window.onload = () => {
         }, 
         false
     )
-    setTimeout(animate, 500)
+    setTimeout(animate, 200)
 }
 
 function init() {
@@ -126,25 +125,21 @@ function init() {
     scene.add( axesHelper );
     // Defines the camera view: FOV, aspect ratio, near, far clipping planes
     const camera = new THREE.PerspectiveCamera(
-        60, window.innerWidth / window.innerHeight,
-        0.1, 10000
+        70, window.innerWidth / window.innerHeight,
+        0.1, 100000
     );
 
     // Instantiate the renderer object
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth - 100, window.innerHeight - 200);
-    document.body.appendChild(renderer.domElement);
-    
     renderer.setClearColor( 0x000, 1)
+    document.body.appendChild(renderer.domElement);
 
     const light = new THREE.AmbientLight( 0xffffff, 3 ); // soft white light
     scene.add( light );
 
-    camera.position.z = 700;
-    camera.position.x = 0;
-    camera.position.y = 0;
-
-    var ship = new Ship(loader);
+    const ship = new Ship(loader);
+    
     renderer.render( scene, camera );
 
     return { scene, camera, renderer, loader, ship };
@@ -153,34 +148,36 @@ function init() {
 function addSphere(){
 
     // The loop will move from z position of -1000 to z position 1000, adding a random particle at each position. 
-    for ( let i = -10000; i < 10000; i += 20 ) {
+    for ( let i = 0; i < 2000; i ++ ) {
 
         // Make a sphere (exactly the same as before). 
-        var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
-        var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-        var sphere = new THREE.Mesh(geometry, material)
+        let geometry   = new THREE.SphereGeometry(0.5, 32, 32)
+        let material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+        let sphere = new THREE.Mesh(geometry, material)
 
         // This time we give the sphere random x and y positions between -500 and 500
         sphere.position.x = Math.random() * 10000 - 5000;
         sphere.position.y = Math.random() * 10000 - 5000;
 
         // Then set the z position to where it is in the loop (distance of camera)
-        sphere.position.z = i;
-
+        sphere.position.z = Math.random() * 10000 - 5000;
         // scale it up a bit
         sphere.scale.x = sphere.scale.y = 5;
+        if(i == 0) {
+            sphere.scale.x = sphere.scale.y = 100;
+        }
 
-        //add the sphere to the scene
-        scene.add( sphere );
-
-        //finally push it to the stars array 
-        stars.push(sphere); 
+        starGroup.add(sphere)
+        stars.push( sphere );
     }
+    
 }
 
 function animate() {
-    ship.drift()
-    requestAnimationFrame( animate );
+    //ship.drift()
+    
+    ship.ship.getWorldDirection(direction)
+    camera.up.set(direction.x, direction.y, direction.z)
     renderer.render( scene, camera );
+    requestAnimationFrame( animate );
 }
-addSphere();
